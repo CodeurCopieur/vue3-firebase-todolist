fed21d0db326d01d8b0e601fb4b0c04bb3f69698
Widdy LOUIS <louisws@outlook.fr>
2023-08-29 15:25:59 +0200

    Todos Reactive

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,14 @@
 <script setup>
+  /* imports */
   import { Icon } from '@iconify/vue'
   import { ref, reactive } from 'vue'
+
+  /* todos */
+  const state = reactive({
+    todos: [{id:'id1', content: 'content 1', done: false}, {id:'id2', content: 'content 2', done: true}]
+  });
+
+  const { todos } = state;
 </script>
 
 <template>
@@ -8,7 +16,7 @@
     <div class="bg-white p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
       <div class="mb-4">
         <h1 class="text-grey-darkest">Todo List</h1>
-        <div class="flex mt-4">
+        <div class="flex">
           <input class="appearance-none outline-none border border-t-0 border-x-0 w-full py-3 px-3 mr-4 text-grey-darker"
             placeholder="Ajouter Todo">
           <button
@@ -16,16 +24,21 @@
         </div>
       </div>
       <div>
-        <div class="flex mb-4 items-center">
-          <p class="w-full text-grey-darkest">Add another component to Tailwind Components</p>
-          <button class="flex-no-shrink p-2 ml-4 mr-2 border-none bg-white hover:text-white text-green hover:bg-green">
-            <Icon icon="lucide:check" class="text-xl" />
-          </button>
-          <button class="flex-no-shrink p-2 ml-2 border-none bg-white text-red hover:text-white hover:bg-red">
-            <Icon icon="lucide:trash-2" class="text-xl" />
-          </button>
+        <div 
+            v-for="todo in todos"
+            :key="todo.id"
+            class="flex mb-4 items-center">
+              <p 
+                :class="{'line-through' : !todo.done}"
+                class="w-full text-grey-darkest"> {{ todo.content }}</p>
+              <button class="flex-no-shrink cursor-pointer p-2 ml-4 mr-2 border-none bg-white hover:text-white text-green hover:bg-green">
+                <Icon :icon="todo.done ? 'lucide:check' : 'ant-design:close-outlined'" class="text-xl" />
+              </button>
+              <button class="flex-no-shrink cursor-pointer p-2 ml-2 border-none bg-white text-red hover:text-white hover:bg-red">
+                <Icon icon="lucide:trash-2" class="text-xl" />
+              </button>
         </div>
-        <div class="flex mb-4 items-center">
+        <!-- <div class="flex mb-4 items-center">
           <p class="w-full line-through text-green">Submit Todo App Component to Tailwind Components</p>
           <button class="flex-no-shrink p-2 ml-4 mr-2 border-none bg-white hover:text-white text-green hover:bg-green">
             <Icon icon="ant-design:close-outlined" class="text-xl" />
@@ -33,7 +46,7 @@
           <button class="flex-no-shrink p-2 ml-2 border-none bg-white text-red hover:text-white hover:bg-red">
             <Icon icon="lucide:trash-2" class="text-xl" />
           </button>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
